3d3fef6fb91f60f99889032e421d37c738d8274e
Nexa <nexa1024@outlook.com>
2026-01-15 11:29:19 +0800

    init: skeleton

---
+++ app/src/main/java/com/nexa/perfstabilitylab/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.nexa.perfstabilitylab
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.Placeholder
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
+import com.google.android.material.appbar.MaterialToolbar
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        // 设置ActionBar
+        val toolBar = findViewById<MaterialToolbar>(R.id.toolbar)
+        setSupportActionBar(toolBar)
+
+        // 把状态栏/刘海区域的高度加到 toolbar 的 top padding
+        val initialTopPadding = toolBar.paddingTop
+        ViewCompat.setOnApplyWindowInsetsListener(toolBar) { v, insets ->
+            val topInset = insets.getInsets(WindowInsetsCompat.Type.statusBars()).top
+            v.updatePadding(top = initialTopPadding + topInset)
+            insets
+        }
+        ViewCompat.requestApplyInsets(toolBar)
+
+        // 先设置左上角返回按钮点击事件，day2再做显示隐藏逻辑
+        toolBar.setNavigationOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.main_container, PlaceholderFragment())
+                .commit()
+        }
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,30 @@
+pluginManagement {
+    repositories {
+        // 插件新加3行
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        // 库加2行
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "PerfStabilityLab"
+include(":app")
